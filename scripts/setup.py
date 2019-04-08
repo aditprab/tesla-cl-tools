c5d2f584c6cc0841677c0819d74c47ea17f3d98f
Adithya <aprabhakaran@scu.edu>
2019-04-08 01:01:33 -0700

    Print something to let the user know set up was successful

--- scripts/setup.py
+++ scripts/setup.py
@@ -9,4 +9,6 @@ import requests
 def setup():
     print("Starting set up process. Your login information is only used to connect to Tesla auth once, and is not stored locally.")
     (email,password) = prompt.promptLogin()
-    authTokenGenerator.fetchAuthToken(email, password)
+    authToken = authTokenGenerator.fetchAuthToken(email, password)
+    if authToken is not None:
+        print("Auth token was successfully generated, you can now call other commands")
